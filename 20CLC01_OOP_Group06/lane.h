6ccdfbfbeb852563b5744a083004a59ac0eb57e5
PHoaiLe <lehoaiphuong13042002@gmail.com>
2021-11-30 15:58:12 +0700

    them class lane

--- 20CLC01_OOP_Group06/lane.h
+++ 20CLC01_OOP_Group06/lane.h
@@ -5,8 +5,11 @@
 #include "animal.h"
 #include "light.h"
 #include <vector>
+#include "console.h"
+#include <iostream>
 using namespace std;
 
+/*
 class lane
 {
 private:
@@ -16,7 +19,7 @@ private:
 	light tLight;
 	bool _light;
 public:
-	lane();
+	//lane();
 	lane(bool type = true, bool _light = false);
 	void drawRoad();
 	void drawRoadside();
@@ -25,6 +28,89 @@ public:
 	void updatePosAnimal();
 	void updateTraffic();
 };
+*/
+
+
+static const int LENGTH = 128; // thu nghiem
+	
+
+class Map
+{
+private:
+
+public:
+	virtual void draw(int x, int y, int step, int start_index) = 0;
+	virtual int LengthOfLane() = 0;
+	virtual void Erase(int x, int y) = 0;
+	virtual void Light_execution(int x, int y, int lane_index, int& pos) = 0;
+	virtual void LoadCharacter() = 0;
+};
+
+
+class Map1 : public Map
+{
+private:
+	vector<pair<int, pair<int,string>>> RawLane; /*=
+	{
+		{0,"                    0000000000                      0000000000                     0000000000                          0000000000                      0000000000"}, //0
+		{0,"                    0000000000                      0000000000                     0000000000                          0000000000                      0000000000"}, //1
+		{-2,"             XXXX      XXXX                   XXXX XXXX XXXX             XXXX          XXXX XXXX   XXXX     XXXX XXXX                     XXXX      XXXX         "}, //2
+		{-2,"             XXXX      XXXX                   XXXX XXXX XXXX             XXXX          XXXX XXXX   XXXX     XXXX XXXX                     XXXX      XXXX         "}, //3
+		{1,"     BBBBBBBBBBBB                  BBBBBBBBBBBB               BBBBBBBBBBBB               BBBBBBBBBBBB          BBBBBBBBBBBB                     BBBBBBBBBBBB     "}, //4
+		{1,"     BBBBBBBBBBBB                  BBBBBBBBBBBB               BBBBBBBBBBBB               BBBBBBBBBBBB          BBBBBBBBBBBB                     BBBBBBBBBBBB     "}, //5
+		{0,"                             0000000000                       0000000000                 0000000000                 0000000000                  0000000000       "}, //6
+		{0,"                             0000000000                       0000000000                 0000000000                 0000000000                  0000000000       "}, //7
+		{2,"    XXXX                  XXXX              XXXX        XXXX             XXXX          XXXX                                      XXXX                            "}, //8
+		{2,"    XXXX                  XXXX              XXXX        XXXX             XXXX          XXXX                                      XXXX                            "}, //9
+		{0,"                                                                                                                                                                 "}, //10
+		{-2,"              DDD                               DDD       CCC                        CCC        DDD                          DDD DDD                             "}, //11
+		{0,"                                                                                                                                                                 "}, //12
+		{0,"                                                                                                                                                                 "} //13
+	};*/   
+	//         <velocity, {color, string}>
+	string eraseStr;
+	vector<pair<bool, int>> Light;
+	/*{
+		{true,-1}, {true,-1}, {true,105}, {true,105}, {true,-1}, {true,-1}, {true,-1}, {true,-1}, {true,-1}, {true,-1}, {true,-1}, {true,-1}, {true,-1}, {true,-1}
+	};*/
+	
+	int Stoptime = 18; // khoang 2s
+
+	vector<vehicle*> vehicleArr;
+	vector<animal*> animalArr;
+
+	// RawLane[i].first = velocity.         RawLane[i].first < 0-> velocity > 0. RawLane[i].first < 0 -> velocity < 0
+public:
+	Map1();
+	void draw(int x, int y, int step, int start_index);
+	int LengthOfLane();
+	void Erase(int x, int y);
+	void Light_execution(int x, int y, int lane_index, int& pos);
+	void LoadCharacter();
+};
+
+
+class lane
+{
+private:
+	Map* cur_state;
+
+public:
+	lane();
+	void TransitionTo(Map* state);
+	void DrawLane(int x, int y, int step, int _start_index);
+	void Erase(int x, int y);
+	int LenOfLane();
+	void LoadCharacter();
+	~lane()
+	{
+		delete cur_state;
+	}
+
+};
+
+
+
 
 #endif // !_lane_h
 
